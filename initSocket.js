28f1104a7e44312c60eda85bfa2503f256b1d9a9
Mpass <121707487+ME942082@users.noreply.github.com>
2025-06-26 21:38:59 +0800

    加強socket.io穩定性

--- initSocket.js
+++ initSocket.js
@@ -7,6 +7,8 @@ export default function initSocket(server) {
     reconnection: true,
     reconnectionAttempts: 5,
     reconnectionDelay: 1000,
+    reconnectionDelayMax: 5000,
+    timeout: 60000,
     cors: {
       origin: ["https://meow-chat-vercel.vercel.app", "http://localhost:5173"],
       credentials: true,
@@ -15,8 +17,13 @@ export default function initSocket(server) {
 
   io.on("connection", (socket) => {
     const userId = socket.handshake.query.userId;
-
     io.emit("user-status-online", userId);
+
+    socket.on("ping", () => {
+      console.log("I am alive");
+    });
+
+    
 
     socket.on("join room", (room_id) => {
       socket.join(room_id);
