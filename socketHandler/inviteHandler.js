214f444d5ad3ef6bbb40e7ee7683b2570fdde182
Mpass <121707487+ME942082@users.noreply.github.com>
2025-07-15 20:32:49 +0800

    修正邀請功能

--- socketHandler/inviteHandler.js
+++ socketHandler/inviteHandler.js
@@ -1,6 +1,6 @@
 export default function inviteHandler(socket, io, userSocketMap) {
   socket.on("send group invite", (chatroom, sender, targetUser_id) => {
-    const targetSocketId = userSocketMap.get(targetUser_id);
+    const targetSocketId = userSocketMap.get(targetUser_id)?.socketId;
     if (!targetSocketId) return;
     io.to(targetSocketId).emit(
       "send group invite",
@@ -10,7 +10,7 @@ export default function inviteHandler(socket, io, userSocketMap) {
     );
   });
 
-  socket.on("invite accepted", (chatroom, targetUser_id) => {
-    io.emit("invite accepted", chatroom, targetUser_id);
+  socket.on("invite accepted", (chatroom) => {
+    socket.broadcast.emit("invite accepted", chatroom);
   });
 }
--- socketHandler/roomHandler.js
+++ socketHandler/roomHandler.js
@@ -1,11 +1,11 @@
 export default function roomHandler(socket, io) {
   socket.on("join room", (room_id) => {
-    //console.log("join room", room_id);
+    console.log("join room", room_id);
     socket.join(room_id);
   });
 
   socket.on("leave room", (room_id) => {
-    //console.log("leave room", room_id);
+    console.log("leave room", room_id);
     socket.leave(room_id);
   });
 }
